99f39348f98ad6b9ec8a3733281a0511e8f0a405
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-06-06 11:35:41 -0300

    criada tela de adicionar uma nova entrada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/services/async_study.dart';
 import 'screens/home_screen/home_screen.dart';
 import 'services/journal_service.dart';
@@ -22,17 +24,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
         appBarTheme: const AppBarTheme(
-          elevation: 0,
-          backgroundColor: Colors.black,
-          titleTextStyle: TextStyle(color: Colors.white),
-        ),
+            elevation: 0,
+            backgroundColor: Colors.black,
+            titleTextStyle: TextStyle(color: Colors.white),
+            //iconTheme: IconThemeData(color: Colors.white),
+            actionsIconTheme: IconThemeData(color: Colors.white)),
         //textTheme: GoogleFonts.bitterTextTheme(),
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      initialRoute: "home",
+      //initialRoute: "home",
+      initialRoute: "add-journal",
       routes: {
         "home": (context) => const HomeScreen(),
+        "add-journal": (context) => AddJournalScreen(
+              journal: Journal(
+                  id: "id",
+                  content: "content",
+                  createdAt: DateTime.now(),
+                  updatedAt: DateTime.now()),
+            ),
       },
     );
   }
