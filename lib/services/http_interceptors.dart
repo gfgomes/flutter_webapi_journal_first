2d99f4c3324bb3a7fc6b0d72a446f25ef00ad02e
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-06-06 10:52:07 -0300

    Adicionado logger nos interceptadores de requisição

--- lib/services/http_interceptors.dart
+++ lib/services/http_interceptors.dart
@@ -1,17 +1,27 @@
 import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
 
 class LoggerInterceptor implements InterceptorContract {
+  var logger = Logger();
+
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
     print("----- Request -----");
     print(data.toString());
+    logger.v(
+        "Requisição para ${data.baseUrl}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
-    print("----- Response -----");
-    print(data.toString());
+    if (data.statusCode ~/ 100 == 2) {
+      logger.i(
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
+    } else {
+      logger.e(
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
+    }
     return data;
   }
 }
